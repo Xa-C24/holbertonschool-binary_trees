3f60756de7d27bf69b495b03e4a1d00d1d285095
Xa-C24 <9335@holbertonstudents.com>
2024-08-07 09:15:14 +0200

    Write a function that goes through a binary tree using in-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,11 +8,11 @@
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-		return;							 /* Base case: if tree is NULL or func is NULL, return */
+		return;							 /* Base case: if tree is NULL or func is NULL, return. */
 
 	binary_tree_inorder(tree->left, func);
-	func(tree->n);						 /* Recursively traverse the left subtree */
+	func(tree->n);						 /* Recursively traverse the left subtree. */
 
 	binary_tree_inorder(tree->right, func);
-	/* Recursively traverse the right subtree */
+	/* Recursively traverse the right subtree. */
 }
